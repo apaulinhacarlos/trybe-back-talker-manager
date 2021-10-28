caa85cf2e4ec5def119121764cd5bed118bee61b
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-10-28 16:27:43 -0300

    Corrige erros

--- middlewares/createTalker.js
+++ middlewares/createTalker.js
@@ -12,13 +12,14 @@ const getDataTalker = async (req, res) => {
     talk: validateTalk(req, res),
   };
 
-  return { readFile, data };
+  return data;
 };
 
 const createTalker = async (req, res, next) => {
   try {
-    const content = getDataTalker(req, res);
-    await fs.writeFileSync('./talker.json', JSON.stringify([...content.readFile, content.data]));
+    const readFile = await JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+    const content = await getDataTalker(req, res);
+    await fs.writeFileSync('./talker.json', JSON.stringify([...readFile, content]));
     res.status(201).json(content);
   } catch (err) {
     next(err);
--- middlewares/getTalkerById.js
+++ middlewares/getTalkerById.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const getTalkerById = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const readFile = await JSON.parse(fs.readreadFileSync('./talker.json', 'utf-8'));
+    const readFile = await JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
     const talkerById = await readFile.find((talker) => talker.id === Number(id));
     if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     res.status(200).json(talkerById);
